b58c6125b702d541fd94ac5f1796c6fee54a91db
Yuri Margaryan <yuri.margaryan@tumo.org>
2025-07-09 14:03:59 +0400

    Add files via upload

---
+++ das/das20/index.php
@@ -0,0 +1,48 @@
+<?php
+function functionName(){
+$animals1 = array("Lion", "Eagle", "Tiger");
+$animals2 = array("Unicorn", "Pegas", "Dragon", "Chopacabra");
+$LOR = array("Hobbit", "Elf", "Gnome", "Goblin");
+echo "<ul>";
+foreach ($animals1 as $key => $value) {
+}
+echo "<li>$value</li>";
+echo "</ul>";
+echo "<ul>";
+foreach ($animals2 as $key => $value) {
+}
+echo "<li>$value</li>";
+echo "</ul>";
+echo "<ul>";
+foreach ($LOR as $key => $value) {
+}
+echo "<li>$value</li>";
+echo "</ul>";
+function functWithReturn($a,$b)
+{
+}
+return $a." ".$b;
+function functWithoutArg()
+{
+echo "smth";
+}
+functWithoutArg();
+}
+$x = 4;
+function test() {
+}
+echo $x;
+test();
+echo $x;
+$x = 4;
+test();
+echo "<br>";
+test();
+echo "<br>";
+test();
+echo "<br>";
+VALUES
+();
+VALUES
+('unp', 'Uwpn', 200000);
+?>
